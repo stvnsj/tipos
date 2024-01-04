73b7136fbc0663022c644c7be49321d2c0a20108
Jonnathan Stevens <stvns.j@proton.me>
2024-01-04 04:42:50 -0300

    More tests

--- my-tests.rkt
+++ my-tests.rkt
@@ -48,33 +48,6 @@ la función aplicada. El tipo resultante de una
 aplicación es el tipo de retorno de la función
 aplicada.
 
-Para los errores:
-
-- Los errores de identificadores libres (o
-funciones no definidas). Este error debe
-detectarse estáticamente.
-
-- Los mensajes de error de tipo detectados
-estáticamente tienen que seguir el siguiente
-patrón:
-
------|
-P3   |
------|
-
-- En el intérprete, cuando se efectúa una
-aplicación de función, se debe que verificar que
-los argumentos cumplan con los contratos
-declarados (en caso de que existan).  
-
-- Cuando el contrato no se cumpla, se debe lanzar
-un error ...
-
-- Una función usada como contrato debe aceptar un
-solo argumento de cualquier tipo válido y debe
-retornar un valor de tipo Bool. En caso de no
-cumplir esta condición se debe lanzar un error ...
-
 |#
 
 
@@ -85,31 +58,35 @@ cumplir esta condición se debe lanzar un error ...
 (require "deftype.rkt")
 
 
-(define-syntax run-test
+(define-syntax test-macro
   (syntax-rules ()
-    ((run-test bool program expected)
-     (if bool (test (run program) expected) (displayln ">> Test Omitted\n")))
-    ((run-test bool description program expected)
-     (if bool (begin
-                (displayln (format "Description: ~a" description))
-                (test (run program) expected))
-         (displayln ">> Test Omitted\n")))))
+    ((test-macro bool program expected)
+     (if bool (test (run program) expected) (displayln ">> Test Omitted")))
+
+    ;; If exc argument is given, an error test is run
+    ((test-macro bool exc program expected)
+     (if bool (test/exn (run program) expected) (displayln ">> Test Omitted")))))
+    
+
+
+    
 
 
 
-(run-test #f "This is some description" '{
+
+(test-macro #f '{
                {define {f {x : Num}} : Num {with {{y 1}} {+ x y}}}
                {with {{y : Num 0}} {f 1}}
                }
           (numV 2))
 
-(run-test #f '{{define {xor {p : Bool} {q : Bool}} : Bool
+(test-macro #f '{{define {xor {p : Bool} {q : Bool}} : Bool
                  { || { && p {! q}} {&& {! p} q}}}
                {xor #t #f}
                }
           (boolV #t))
 
-(run-test #f '{{define {fib {n : Num}} : Num
+(test-macro #f '{{define {fib {n : Num}} : Num
                  {if {= n 0} 0
                      {if {= n 1} 1
                          {+ {fib {- n 1}} {fib {- n 2}}}}}}
@@ -118,22 +95,59 @@ cumplir esta condición se debe lanzar un error ...
 
 
 
+
+;; Different branch types.
+(test-macro #t 1
+            '{{define {and3 {b1 : Bool} {b2 : Bool} {b3 : Bool}} : Bool {&& b1 {&& b2 b3}}}
+              {define {add3 {n1 : Num} {n2 : Num} {n3 : Num}} {+ n1 {+ n2 n3}}}
+              {if #t {and3 #t #t #f} {add3 4 1 -5}}}
+            "Static type error: branches must have same type")
+
+
+
+
+
 ;; =========== Contract Tests ====================
 
-(run-test #t '{
-
-               {define {positive {n : Num}} : Bool {< 0 n}}
-               {define {pitagorean-triple {a : Num @ positive}
-                                          {b : Num }
-                                          {c : Num }} : Bool
-                                          {= {+ {* a a} {* b b}} {* c c}}}
-
-
-
-               {pitagorean-triple 3 4 5}
-               
-               }
+;; All arguments satisfy contract.
+(test-macro #f
+          '{{define {positive {n : Num}} : Bool {< 0 n}}
+            {define {pitagorean-triple {a : Num @ positive}
+                                       {b : Num @ positive}
+                                       {c : Num @ positive}} : Bool
+              {= {+ {* a a} {* b b}} {* c c}}}
+            {pitagorean-triple 3 4 5}}
           (boolV #t))
+
+;; Contract is not satisfied by -5.
+(test-macro #f 1
+          '{{define {positive {n : Num}} : Bool {< 0 n}}
+            {define {pitagorean-triple {a : Num @ positive}
+                                       {b : Num @ positive}
+                                       {c : Num @ positive}} : Bool
+              {= {+ {* a a} {* b b}} {* c c}}}
+            {pitagorean-triple 3 4 -5}}
+          "Runtime contract error: -5 does not satisfy positive")
+
+;; Contract has bad type; it takes more than one argument.
+(test-macro #f 1
+            '{{define {pred1 {x : Num} {y : Num}} {< {+ x y} 0}}
+              {define {fun1  {n : Num @ pred1}} n}
+              {fun1 2}}
+            "Static contract error: invalid type for pred1")
+
+
+;; Contract has bad type; it returns pair of Number Boolean.
+(test-macro #f 1
+            '{{define {positive {n : Num}} : Bool {< 0 n}}
+              {define {pred1 {x : Num}} : {Pair Num Bool} {cons x {positive x}}}
+              {define {fun1  {b : Bool} {n : Num @ pred1}} {if b n {* -1 n}}}
+              {fun1 #t 1}}
+            "Static contract error: invalid type for pred1")
+
+
+
+
 
 
 
